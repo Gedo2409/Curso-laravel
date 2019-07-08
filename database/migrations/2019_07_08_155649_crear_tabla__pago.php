142919a4bd5886f6edf8dbd5540f882cb68d8ed4
Gedo24096 <gerardo2409.1996uv@gmail.com>
2019-07-08 11:00:26 -0500

    Cree tabla Pago

---
+++ database/migrations/2019_07_08_155649_crear_tabla__pago.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CrearTablaPago extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('Pago', function (Blueprint $table) {
+            $table->bigIncrements('id');
+
+            $table->double('Monto');
+            
+            $table->date('Fechadepago');
+            $table->unsignedBigInteger("alumno_id");
+            $table->foreign('alumno_id')->references('id')->on('usuario')->onDelete('restrict')->onUpdate('restrict');
+
+
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('Pago');
+    }
+}
