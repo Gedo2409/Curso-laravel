152fe4f0b0da02d64882c754bbf346fcc6858bb1
Gedo24096 <gerardo2409.1996uv@gmail.com>
2019-08-21 18:57:01 -0500

    Cambie mayusculas

---
+++ resources/views/frontend/SportCenter.blade.php
@@ -0,0 +1,10 @@
+@extends('frontend.layouts.app')
+@section('title')
+Barezzito
+@endsection
+@section('description', 'UN DJ FENOMENAL, UNA BANDA INCREÍBLE, SUSHI DELICIOSO, DRINKS ESPECTACULARES EN UN LUGAR CÁLIDO Y CONTEMPORANEO. 
+SUSHI + BAR + MÚSICA')
+@section('keywords', 'SUSHI, BAR, DIVERSION, ANTRO, PROMOSIONES,')
+@section('content')
+<div> <h1>PRUEBA 1</h1> </div>
+@endsection
